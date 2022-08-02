3dfb99e86816e79ee50969850ab18edfa5758191
atulyashree <89528856+atulyashree@users.noreply.github.com>
2022-08-02 12:53:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
+import pandas as pd
 import pickle
 
 
 app = Flask(__name__)
 model = pickle.load(open('KNN_Titanic.pkl','rb'))
+dataset = pd.read_csv('train.csv')
+X=dataset.iloc[:,[2,4,5,6,7,9]].values
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler()
+X = sc.fit_transform(X)
+
 
 @app.route('/')
 def home():
